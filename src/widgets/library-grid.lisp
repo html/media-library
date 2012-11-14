03bff03cd819b6738c02f7e52175ab6a8762a479
Olexiy Zamkoviy <olexiy.z@gmail.com>
2012-11-14 10:45:04 +0200

    Fixed #6 from http://oz.titanpad.com/4

--- src/widgets/library-grid.lisp
+++ src/widgets/library-grid.lisp
@@ -203,3 +203,26 @@
            (ps:ps 
              (ps:chain (j-query ".modal") (modal))
              (ps:chain (j-query ".modal-backdrop") (unbind "click") (bind "click" (lambda () nil)))))))))))
+
+(defmethod dataedit-create-new-item-widget ((grid library-grid))
+    (make-instance 'dataform
+     :data (make-instance (dataseq-data-form-class grid))
+     :class-store (dataseq-class-store grid)
+     :ui-state :form
+     :on-cancel (lambda (obj)
+		  (declare (ignore obj))
+		  (dataedit-reset-state grid)
+		  (throw 'annihilate-dataform nil))
+     :on-success (lambda (obj)
+                   (safe-funcall (dataedit-on-add-item grid) grid (dataform-data obj))
+                   (safe-funcall (dataedit-on-add-item-completed grid) grid (dataform-data obj))
+                   (when (or (dataedit-mixin-flash-message-on-first-add-p grid)
+                             (> (dataseq-data-count grid) 1))
+                     (flash-message (dataseq-flash grid)
+                                    (format nil "Added ~A."
+                                            (humanize-name (dataseq-data-class grid)))))
+                   (dataedit-reset-state grid)
+                   (setf (dataseq-sort grid) (cons 'item-created-at :DESC))
+                   (throw 'annihilate-dataform nil))
+     :data-view (dataedit-item-data-view grid)
+     :form-view (dataedit-item-form-view grid)))
